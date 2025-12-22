916faeb165ec40a5e9d106074d7fa1c85ef660a7
Young <rhsiddlsidhd@nate.com>
2025-12-23 04:03:00 +0900

    [등수구하기] 구현 - sort & indexOf

---
+++ src/탐색/등수구하기/index.js
@@ -0,0 +1,15 @@
+function solution(arr) {
+  let answer = "",
+    n = arr.length;
+
+  const sorted = arr.slice().sort((a, b) => b - a);
+
+  for (let i = 0; i < n; i++) {
+    answer += arr.indexOf(sorted[[i]]) + 1;
+  }
+
+  return answer;
+}
+
+let arr = [87, 89, 92, 100, 76];
+console.log(solution(arr));
---
+++ src/탐색/등수구하기/solution.md
@@ -0,0 +1,16 @@
+# Problem Submission
+
+- **problem_name**: 등수 구하기
+- **platform**: 알고리즘서적
+- **data_structure**: Array
+- **used_algorithm**
+  > for문
+  > 정렬
+
+### idea_summary
+
+> 정렬배열 과 기존배열 비교
+
+### next_hint
+
+- indexOf
